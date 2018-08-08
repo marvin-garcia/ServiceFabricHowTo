9b5bd64587cd527694b356555cda54e7e400f610
Marvin Garcia <magar@microsoft.com>
2018-08-08 12:12:18 -0400

    Added list files output to py app

--- Docker/app.py
+++ Docker/app.py
@@ -1,11 +1,18 @@
-﻿from flask import Flask
+﻿from os import walk
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 
-    return 'Hello World!'
+    path = '\\\\servicefabric3storage.file.core.windows.net\\fileshare'
+    filestring = ""
+    for root, dirs, files in walk(path):
+        for filename in files:
+            filestring = ", %s" % (filename)
+
+    return filestring
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
