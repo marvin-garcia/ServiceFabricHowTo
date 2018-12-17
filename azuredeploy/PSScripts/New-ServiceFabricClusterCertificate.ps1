9740154fe4641d22f642bda0a2a88e551b70775f
Marvin Garcia <magar@microsoft.com>
2018-12-17 12:22:49 -0600

    added new project to test stateful service. moved applications inside each corresponding project.

--- azuredeploy/PSScripts/New-ServiceFabricClusterCertificate.ps1
+++ azuredeploy/PSScripts/New-ServiceFabricClusterCertificate.ps1
@@ -1,42 +1,43 @@
-<#
-Requires -Module AzureRM.KeyVault
+﻿Function New-SerficeFabricClusterCertificate
+{
+    Param(
+        [String]$Password,
+        [String]$KeyVaultName,
+        [String]$ClusterName,
+        [String]$CertName = $ClusterName + "ClusterCert",
+        [String]$location,
+        [String]$CertDNSName = $ClusterName + "." + $location + ".cloudapp.azure.com",
+        [String]$PFXFileLocation,
+        [String]$CertStoreLocation = "Cert:\CurrentUser\My"
+    )
 
-Use this script to create a certificate that you can use to secure a Service Fabric Cluster
-This script requires an existing KeyVault that is EnabledForDeployment.  The vault must be in the same region as the cluster.
-Once the certificate is created and stored in the vault, the script will provide the parameter values needed for template deployment
-#>
+    $CertFileFullPath = [System.IO.Path]::Combine($PFXFileLocation, + "$($ClusterName)-Cluster-Cert.pfx")
+    
+    #Create the cluster key
+    $SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+    $NewCert = New-SelfSignedCertificate -CertStoreLocation $CertStoreLocation -DnsName $CertDNSName -Verbose -ErrorAction Stop
+    Export-PfxCertificate -FilePath $CertFileFullPath -Password $SecurePassword -Cert $NewCert -Verbose -ErrorAction Stop
 
-param(
-    [string] [Parameter(Mandatory=$true)] $Password,
-    [string] [Parameter(Mandatory=$true)] $CertName,
-    [string] [Parameter(Mandatory=$true)] $CertDNSName,
-    [string] [Parameter(Mandatory=$true)] $CertFileFullPath,
-    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
-    [string] [Parameter(Mandatory=$true)] $KeyVaultSecretName
-)
+    $Bytes = [System.IO.File]::ReadAllBytes($CertFileFullPath)
+    $Base64 = [System.Convert]::ToBase64String($Bytes)
 
-$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
-$NewCert = New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName $CertDNSName -Verbose -ErrorAction Stop
-Export-PfxCertificate -FilePath $CertFileFullPath -Password $SecurePassword -Cert $NewCert -Verbose -ErrorAction Stop
+    $JSONBlob = @{
+        data = $Base64
+        dataType = 'pfx'
+        password = $Password
+    } | ConvertTo-Json
 
-$Bytes = [System.IO.File]::ReadAllBytes($CertFileFullPath)
-$Base64 = [System.Convert]::ToBase64String($Bytes)
+    $ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($JSONBlob)
+    $Content = [System.Convert]::ToBase64String($ContentBytes)
 
-$JSONBlob = @{
-    data = $Base64
-    dataType = 'pfx'
-    password = $Password
-} | ConvertTo-Json
+    $SecretValue = ConvertTo-SecureString -String $Content -AsPlainText -Force
+    $VaultCert = Import-AzureKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -FilePath $CertFileFullPath -Password $SecurePassword -Verbose -ErrorAction Stop
 
-$ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($JSONBlob)
-$Content = [System.Convert]::ToBase64String($ContentBytes)
+    Write-Host
+    Write-Host "Cert local path: $($CertFileFullPath)"
+    Write-Host "Cert Thumbprint : $($NewCert.Thumbprint)"
+    Write-Host "Cert URL : $($VaultCert.Id)"
+    Write-Host "Secret URL: $($VaultCert.SecretId)"
 
-$SecretValue = ConvertTo-SecureString -String $Content -AsPlainText -Force
-#$NewSecret = Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -SecretValue $SecretValue -Verbose -ErrorAction Stop
-$VaultCert = Import-AzureKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -FilePath $CertFileFullPath -Password $SecurePassword -Verbose -ErrorAction Stop
-
-Write-Host
-Write-Host "Cert local path: $($CertFileFullPath)"
-Write-Host "Source Vault Resource Id: "$(Get-AzureRmKeyVault -VaultName $KeyVaultName).ResourceId
-Write-Host "Certificate URL : "$VaultCert.Id
-Write-Host "Certificate Thumbprint : "$NewCert.Thumbprint
+    return
+}
